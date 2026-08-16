d61d05c92687082e70e4a5b0997e19ac1ccb8cb3
Marcus Green <marcusavgreen@gmail.com>
2026-08-16 23:38:50 +0100

    chore: bump version to 0.1.14 (inline embed + kebab action)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_reportsources';
-$plugin->release   = '0.1.13';
-$plugin->version   = 2026081300;
+$plugin->release   = '0.1.14';
+$plugin->version   = 2026081600;
 $plugin->requires  = 2024100100; // Moodle 4.5+ for stable Reportbuilder API.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 502];
